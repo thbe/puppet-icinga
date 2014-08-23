a5dd91ab2ce28ccdcac4bd66314d3a9155da20f2
thbe <project@bendler-net.de>
2014-08-23 16:27:20 +0200

    Fixed space in mysql parameter

--- files/schema/populate_icinga_schema.sh
+++ files/schema/populate_icinga_schema.sh
@@ -8,6 +8,8 @@ set -e
 
 # Set MySQL parameter
 MYSQL_COMMAND="/usr/bin/mysql"
+MYSQL_USER="root"
+MYSQL_PASSWORD=${1:-0nly4install}
 
 # Define schema
 SCHEMA_IDO=$(ls -1 /usr/share/doc/icinga2-ido-mysql-*/schema/mysql.sql)
@@ -15,16 +17,16 @@ SCHMEA_WEB=$(ls -1 /usr/share/doc/icinga-web-*/schema/mysql.sql)
 
 # Populate icinga ido schema
 if [ ! -f /etc/sysconfig/mysqldb_icinga ]; then
-  ${MYSQL_COMMAND} icinga < ${SCHEMA_IDO}
-  if [ ${} -eq 0 ]; then
+  ${MYSQL_COMMAND} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} icinga < ${SCHEMA_IDO}
+  if [ ${?} -eq 0 ]; then
     echo "true" > /etc/sysconfig/mysqldb_icinga
   fi
 fi
 
 # Populate icinga web schema
 if [ ! -f /etc/sysconfig/mysqldb_icinga ]; then
-  ${MYSQL_COMMAND} icinga_web < ${SCHEMA_WEB}
-  if [ ${} -eq 0 ]; then
+  ${MYSQL_COMMAND} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} icinga_web < ${SCHEMA_WEB}
+  if [ ${?} -eq 0 ]; then
     echo "true" > /etc/sysconfig/mysqldb_icinga_web
   fi
 fi
